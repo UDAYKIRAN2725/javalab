0b3dd400ed95ba4136a57e42a8afb01cc5c36e83
UDAYKIRAN2725 <116643279+UDAYKIRAN2725@users.noreply.github.com>
2023-04-17 12:47:41 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+import p1.P;
+import p2.Q;
+import p1.R;
+import p3.S;
+public class Test
+{
+	public static void main(String args[])
+	{
+		P p=new P();
+		Q q=new Q();
+		R r=new R();
+		S s=new S();
+	}
+}
+
